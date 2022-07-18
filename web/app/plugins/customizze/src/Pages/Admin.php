5d77091fc9f051614b956d15c1792631927febec
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-07-18 15:29:55 -0300

    Create a API class to create pages with modularity

--- web/app/plugins/customizze/src/Pages/Admin.php
+++ web/app/plugins/customizze/src/Pages/Admin.php
@@ -2,6 +2,8 @@
 
 namespace Cenarioweb\Customizze\Pages;
 
+use Cenarioweb\Customizze\Api\SettingsApi;
+
 /**
  * Menage plugin links and settings pages.
  *
@@ -10,29 +12,39 @@ namespace Cenarioweb\Customizze\Pages;
  */
 class Admin
 {
+    protected $settings;
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
+    }
+
     public function register()
     {
-        add_action('admin_menu', array($this, 'add_admin_page'), 9);
-
-        add_filter('plugin_action_links_' . PLUGIN_BASENAME, array($this, 'add_settings_link'));
+        $this->settings->addPages($this->pages())->register();
     }
 
-    public function add_admin_page()
+    private function pages()
     {
-        add_menu_page('Customizze', 'Customizze', 'manage_options', 'customizze', array($this, 'admin_page' ), 'dashicons-coffee', 110);
-    }
-
-    public function add_settings_link($links)
-    {
-        $settings_link = sprintf('<a href="admin.php?page=%s">%s</a>', PLUGIN_NAME, __('Settings'));
-
-        array_push($links, $settings_link);
-
-        return $links;
-    }
-
-    public function admin_page()
-    {
-        require_once PLUGIN_PATH . 'templates/index.php';
+        return [
+            [
+                'page_title' => 'Customizze - Configurações',
+                'menu_title' => 'Customizze',
+                'capability' => 'manage_options',
+                'menu_slug' => 'customizze',
+                'callback' => function () { echo '<h1>Customizze</h1>'; },
+                'icon_url' => 'dashicons-coffee',
+                'position' => 110
+            ],
+            [
+                'page_title' => 'Alfred - Configurações',
+                'menu_title' => 'Alfred',
+                'capability' => 'manage_options',
+                'menu_slug' => 'alfred',
+                'callback' => function () { echo '<h1>Alfred</h1>'; },
+                'icon_url' => 'dashicons-admin-settings',
+                'position' => 111
+            ]
+        ];
     }
 }
